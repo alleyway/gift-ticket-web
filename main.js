622b447b99fef2e30c7f566cf7cf40c085581509
Michael Lake <mlake900@gmail.com>
2016-01-20 17:46:37 -0500

    updating web page

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ $(function () {
     $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href').slice(2)).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });

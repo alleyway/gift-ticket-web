0a714ecad9edb4d26694cacaec10501ff36635e8
Michael Lake <mlake900@gmail.com>
2016-03-14 22:36:49 -0400

    reducing js file requests by combining

---
+++ app.js
@@ -0,0 +1,9 @@
+---
+# Don't delete
+---
+
+{% include_relative bower_components/jquery/dist/jquery.min.js %}
+{% include_relative bower_components/bootstrap/dist/js/bootstrap.min.js %}
+{% include_relative bower_components/jquery.easing/js/jquery.easing.min.js %}
+{% include_relative main.js %}
+

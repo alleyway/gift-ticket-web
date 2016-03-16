4f3cb953932447d13690c565127707b129469e65
Michael Lake <mlake900@gmail.com>
2016-03-16 11:46:19 -0400

    Better activation

--- activate.js
+++ activate.js
@@ -3,6 +3,33 @@
 
 $(function() {
 
+    $(window).on("load", function() {
+        $.fn.spin.presets.giftticket = {
+            lines: 13 // The number of lines to draw
+            , length: 28 // The length of each line
+            , width: 14 // The line thickness
+            , radius: 42 // The radius of the inner circle
+            , scale: 0.75 // Scales overall size of the spinner
+            , corners: 1 // Corner roundness (0..1)
+            , color: '#000' // #rgb or #rrggbb or array of colors
+            , opacity: 0.25 // Opacity of the lines
+            , rotate: 0 // The rotation offset
+            , direction: 1 // 1: clockwise, -1: counterclockwise
+            , speed: 1 // Rounds per second
+            , trail: 60 // Afterglow percentage
+            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 2e9 // The z-index (defaults to 2000000000)
+            , className: 'spinner' // The CSS class to assign to the spinner
+            , top: '50%' // Top position relative to parent
+            , left: '50%' // Left position relative to parent
+            , shadow: false // Whether to render a shadow
+            , hwaccel: false // Whether to use hardware acceleration
+            , position: 'absolute' // Element positioning
+        };
+    });
+
+    $("input,select,textarea").not("[type=submit]").jqBootstrapValidation();
+
     $.urlParam = function(name){
         var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
         if (results==null){
@@ -33,6 +60,7 @@ $(function() {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
 
+
     var mobileEntry = $("#mobileEntry");
 
     mobileEntry.intlTelInput({
@@ -41,11 +69,10 @@ $(function() {
     });
 
     var mobile = $.urlParam("mobile");
+
     if (mobile) {
         mobileEntry.intlTelInput("setNumber", mobile);
     }
-
-
 
     var activationCode = $('#activationCode');
 
@@ -64,29 +91,27 @@ $(function() {
     activationCode.attr('autocorrect', 'off');
     activationCode.attr('autocapitalize', 'off');
 
-    if (false){
+    if (true){
         $("input[name='activationCode']").val("555-555");
         $("input[name='name']").val("Michael Lake");
         $("input[name='email']").val("mlake900+test@gmail.com");
         $("input[name='mobileEntry']").val("434-202-9223");
     }
 
+    var activationButton = $("#activation_button");
     var activationForm = $("#activation_form");
 
     activationForm.on("submit", function(e){
+        
+        $('.box').spin('giftticket');
+        activationButton.prop('disabled', true);
+        
+        var activationCode = $("#activationCode");
+        activationCode.parent().toggleClass("floating-label-form-group-with-value", !! activationCode.val());
         e.preventDefault();
-        //$.ajax({
-        //    type:"get",
-        //    url: BASE_URL + "/activation",
-        //    async: true,
-        //    dataType: "json",
-        //    success: function(){
-        //        alert("success");
-        //    }
-        //});
 
         var activationRequest = {
-            "activationCode": $("input[name='activationCode']").val(),
+            "activationCode": activationCode.val(),
             "name" : $("input[name='name']").val(),
             "email" :$("input[name='email']").val(),
             "mobile" : $("input[name='mobileEntry']").val()
@@ -104,6 +129,15 @@ $(function() {
             async: true,
             dataType: "json",
             success: function(data){
+                $('.box').spin(false);
+                activationButton.prop('disabled', false);
+                if (data.success){
+                    
+
+                } else {
+
+                }
+
                 alert(data.message);
             }
         });

5580e7b183e66d1720e420cfc601032425fbb0fb
Michael Lake <mlake900@gmail.com>
2016-04-19 17:48:33 -0400

    Updates to activate

--- activate.js
+++ activate.js
@@ -107,14 +107,29 @@ $(function() {
             success: function(data){
                 $('.box').spin(false);
                 activationButton.prop('disabled', false);
+
+
                 if (data.success){
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>" + data.message + "</strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    //$('#contactForm').trigger("reset");
                     
-
                 } else {
-
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>" + data.message + "</strong>" );
+                    $('#success > .alert-danger').append('</div>');
                 }
-
-                alert(data.message);
             }
         });
 
